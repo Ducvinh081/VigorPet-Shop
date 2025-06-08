b97521a8fa1f9d9a374e0039eb32c7baef7508ed
Ducvinh081 <asasuke098@gmail.com>
2025-06-09 06:08:27 +0700

    update isAdmin
    update

--- src/libs/isAdmin.js
+++ src/libs/isAdmin.js
@@ -1,6 +1,6 @@
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
-import connectDB from "@/lib/connectDB";
+import connectDB from "./mongoConnect";
 import UserInfo from "@/models/UserInfo";
 
 export async function isAdmin() {
