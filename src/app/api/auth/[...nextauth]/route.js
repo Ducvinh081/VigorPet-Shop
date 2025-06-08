0a2114ed95962b562d468f38b6078e3c856bb240
Ducvinh081 <asasuke098@gmail.com>
2025-06-09 06:28:45 +0700

    change link
    a

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,5 @@
 // src/app/api/auth/[...nextauth]/route.js
 import NextAuth from "next-auth";
-import { authOptions } from "@/lib/authOptions";
+import { authOptions } from "@/libs/authOptions";
 
 export default NextAuth(authOptions);
